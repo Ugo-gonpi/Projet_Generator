4a46d420c27057c0bd101ee58f64287ba757f2e2
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-01-19 14:45:34 +0100

    created the functions for the checkmarks

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,52 @@
-function generatePass() {
-    let pass = '';
-    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-        'abcdefghijklmnopqrstuvwxyz0123456789@#$';
- 
-    for (let i = 1; i <= 8; i++) {
-        let char = Math.floor(Math.random()
-            * str.length + 1);
- 
-        pass += str.charAt(char)
+
+let btn = document.querySelector("button")
+
+let input = document.querySelectorAll("div")[1]
+
+let checkBoxes = document.querySelectorAll("input")
+
+//--------Here we create all the functions so our check marks allow us to adapt as we want our password-----------
+
+function chooseLength() {
+    if (checkBoxes[0].checked == true) {
+        
+    } else {
+
     }
- 
-    return pass;
 }
 
-console.log(generatePass());
+function chooseUpper() {
+    if (checkBoxes[1].checked == true) {
+        
+    } else {
+        
+    }
+}
 
-  
+function chooseLower() {
+    if (checkBoxes[2].checked == true) {
+        
+    } else {
+        
+    }
+}
+
+function chooseNumber() {
+    if (checkBoxes[3].checked == true) {
+        
+    } else {
+        
+    }
+}
+
+function chooseSymbol() {
+    if (checkBoxes[4].checked == true) {
+        
+    } else {
+        
+    }
+}
+
+btn.addEventListener("click", () => {
+    
+})
