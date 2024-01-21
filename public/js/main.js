e4fc5d27806f04888cfdd2715657e5bd875fca7c
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-01-21 23:28:04 +0100

    finished the js

--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,93 @@
 
-let btn = document.querySelector("button")
+let longuerPass = document.querySelectorAll("input")[0] 
 
-let input = document.querySelectorAll("div")[1]
+let lettreMaj = document.querySelectorAll("input")[1] 
+let lettreMin = document.querySelectorAll("input")[2] 
+let nombre = document.querySelectorAll("input")[3] 
+let symboles = document.querySelectorAll("input")[4] 
 
-let checkBoxes = document.querySelectorAll("input")
+let clipboardCopy = document.querySelectorAll("button")[0] // <--- bouton pour copier le password
+let generatePassword = document.querySelectorAll("button")[1] // <--- bouton qui génere le password
 
-//--------Here we create all the functions so our check marks allow us to adapt as we want our password-----------
+let passwordDisplay = document.querySelector(".falseInput")
 
-function chooseLength() {
-    if (checkBoxes[0].checked == true) {
-        
-    } else {
+// ----------------------------------------------------------------
+   
+let lesLettres = "abcdefghijklmnopqrstuvwxyz";
+let lettre
 
-    }
-}
+let lesChiffres = "0123456789" 
+let chiffre
 
-function chooseUpper() {
-    if (checkBoxes[1].checked == true) {
-        
-    } else {
-        
-    }
-}
+let lesSymboles = "&@#$£?!%-"
+let symbol
 
-function chooseLower() {
-    if (checkBoxes[2].checked == true) {
-        
-    } else {
-        
-    }
-}
+let motPass = []
 
-function chooseNumber() {
-    if (checkBoxes[3].checked == true) {
-        
-    } else {
-        
-    }
-}
+let currentval = 0
 
-function chooseSymbol() {
-    if (checkBoxes[4].checked == true) {
-        
-    } else {
-        
-    }
-}
+// -----------------------------------------------------------------
 
-btn.addEventListener("click", () => {
+let addLetter = () => {
     
+    lettre = lesLettres[Math.floor(Math.random() * lesLettres.length)];
+
+    if (lettreMaj.checked == true && lettreMin.checked == true) {
+        upperLower =  Math.floor(Math.random() * 2)
+        switch (upperLower) {
+            case 0:
+                motPass.push(lettre.toUpperCase())
+                break;
+            case 1:
+                motPass.push(lettre)
+                break;
+        }
+    } else if (lettreMin.checked == true && lettreMaj.checked == false) {
+        motPass.push(lettre)
+    } else if (lettreMin.checked == false && lettreMaj.checked == true) {
+        motPass.push(lettre.toUpperCase())
+    }
+}
+
+let addNumber = () => {
+    if (nombre.checked == true) {
+        chiffre = lesChiffres[Math.floor(Math.random() * lesChiffres.length)];
+        motPass.push(chiffre)
+    }
+}
+
+let addSymbol = () => {
+    if (symboles.checked == true) {
+        symbol = lesSymboles[Math.floor(Math.random() * lesSymboles.length)];
+        motPass.push(symbol)
+    }
+}
+
+let generate = () => {
+
+    currentval = longuerPass.value
+
+    while (motPass.length < currentval) {
+        let randomCharacter =  Math.floor(Math.random() * 3)
+
+        switch (randomCharacter) {
+            case 0:
+                addLetter()
+                break;
+            case 1:
+                addNumber()
+                break;
+            case 2:
+                addSymbol()
+                break;
+        }
+    }
+}
+
+//------------------------------------------------------------------------
+
+generatePassword.addEventListener("click", () => {
+    password = []
+    generate()
+    passwordDisplay.innerHTML = password.join("")
 })
